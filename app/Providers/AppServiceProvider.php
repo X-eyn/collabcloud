e00bdbd413a97d3908b99b286d7247ab958d8341
x-eyn <zain.ahmed@g.bracu.ac.bd>
2023-07-09 08:21:12 +0600

    Created Objects in DB for Files,Folders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,33 @@
 
 namespace App\Providers;
 
+use App\Models\File;
+use App\Models\Folder;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Relation::morphMap([
+            'folder' => Folder::class,
+            'file' => File::class
+        ]);
     }
 }
