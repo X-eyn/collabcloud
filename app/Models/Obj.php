14dd7014749a9c55d22d259c2e070d5a83500e30
RAY101x <131998895+RAY101x@users.noreply.github.com>
2023-08-06 11:04:32 +0600

    Update for database objects.
    Added uuid, objectable type, parent id, team id. Modifying Models(File, Folder,Obj,Team) code.

--- app/Models/Obj.php
+++ app/Models/Obj.php
@@ -2,13 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Obj extends Model
 {
     use HasFactory;
 
     public $table = 'objects';
+
+    protected $fillable =[
+        'parent_id'
+    ];
+
+    public static function booted()
+    {
+        static::creating(function($model){
+            $model->uuid = Str::uuid(); //creating uuid wth v4
+        });
+    }
+
+    public function objectable()
+    {
+        return $this->morphTo(); 
+    }
     
 }
